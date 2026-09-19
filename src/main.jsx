8c601148be8656de1191e8c33e019cebad2bae30
Lioqueni Macalo <lioquenimacalo@gmail.com>
2026-09-19 11:26:43 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { registerSW } from 'virtual:pwa-register'
 
+// Captura o evento de instalação do PWA
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault()
+  window.__deferredPrompt = e
+  window.dispatchEvent(new Event('pwa-install-available'))
+})
+
 registerSW({
   onOfflineReady() {
     console.log('App pronta para funcionar offline')
